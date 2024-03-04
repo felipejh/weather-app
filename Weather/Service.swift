3d32efb982fb75e9bad76f6773e32dfe7ad002ce
Felipe Hoffmann <felipejh@gmail.com>
2024-03-03 21:09:58 -0300

    Class 06 - create decodable

---
+++ Weather/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  Weather
+//
+//  Created by Felipe Hoffmann on 03/03/24.
+//
+
+import Foundation
+
+class Service {
+    
+    private let baseURL: String = "https://api.openweathermap.org/data/3.0/onecall"
+    private let apiKey: String = "afc4195c7d4ee060476ad31e9d992447"
+    
+    func fetchData(_ completion: () -> Void) {
+        
+    }
+}
